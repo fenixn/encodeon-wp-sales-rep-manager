e5599c20e407d27184f62c62fca936fefd2b4552
Phuong Nguyen <artphuong@live.com>
2018-01-29 18:52:40 -0800

    Added jqvmap to plugin vendor folder. Inserted US map into the frontend view.

--- encodeon-sales-rep-manager/plugin/Plugin.php
+++ encodeon-sales-rep-manager/plugin/Plugin.php
@@ -7,9 +7,7 @@ class Plugin
         $this->run_controllers();
 
         add_action('admin_menu', array($this, 'on_wp_admin_menu'));
-
-        new Views\Frontend\SalesRep\Map;
-
+        add_action('wp_enqueue_scripts', array($this, 'frontend_enqueue_scripts'));
     }
 
     public function on_wp_admin_menu()
@@ -17,6 +15,36 @@ class Plugin
         new Views\Admin\MainMenu;
         new Views\Admin\SalesRep\CreateView;
         new Views\Admin\SalesRep\EditView;
+    }
+
+    public function frontend_enqueue_scripts()
+    {
+        new Views\Frontend\SalesRep\Map;
+
+        // Enqueue Bootstrap. Does a cursory check first to see if a bootstrap handle already exists
+        if((!wp_style_is('jqvmap', 'queue')) && 
+           (!wp_style_is('jqvmap', 'done'))) {
+            wp_enqueue_style(
+                'jqvmap',
+                plugins_url('encodeon-sales-rep-manager/vendor/jqvmap-master/dist/jqvmap.min.css'),
+                array(),
+                '1.5.0'
+            );
+
+            wp_enqueue_script(
+                'jqvmap',
+                plugins_url('encodeon-sales-rep-manager/vendor/jqvmap-master/dist/jquery.vmap.min.js'),
+                array('jquery'),
+                '1.5.0'
+            );
+
+            wp_enqueue_script(
+                'jqvmap-usa',
+                plugins_url('encodeon-sales-rep-manager/vendor/jqvmap-master/dist/maps/jquery.vmap.usa.js'),
+                array('jquery'),
+                '1.5.0'
+            );
+        }
     }
 
     public function run_controllers()
--- encodeon-sales-rep-manager/plugin/Views/Frontend/SalesRep/Map.php
+++ encodeon-sales-rep-manager/plugin/Views/Frontend/SalesRep/Map.php
@@ -9,6 +9,48 @@ class Map
 
     public function render()
     {
-        echo "test";
+        ?>
+        <div id="vmap" class="col-md-12"></div>
+
+        <script type="text/javascript">
+            (function($) {
+                $(document).ready(function() {
+                    function resize_map() {
+                        $('#vmap').height($("#vmap").width()*2/3);
+                    }
+                    resize_map();
+
+                    window.addEventListener("resize", function() {
+                        resize_map();
+                    });
+
+                    $('#vmap').vectorMap({
+                        map: 'usa_en',
+                        backgroundColor: "#FFF",
+                        borderColor: "#FFF",
+                        borderOpacity: 0.5,
+                        borderWidth: 2,
+                        color: '#CCC',
+                        enableZoom: false,
+                        hoverColor: '#39F',
+                        hoverOpacity: null,
+                        normalizeFunction: 'linear',
+                        selectedColor: '#36F',
+                        selectedRegions: null,
+                        showTooltip: true,
+                        onRegionClick: function(element, code, region)
+                        {
+                            var message = 'You clicked "'
+                                + region
+                                + '" which has the code: '
+                                + code.toUpperCase();
+
+                            console.log(message);
+                        }
+                    });
+                });
+            }(jQuery));
+        </script>
+        <?php
     }
 }
